cfe01d6e1ee3ee6ee751adf97eceb49a5bf00cda
Andrianooo <vladosandrianov@yandex.ru>
2024-12-02 15:05:09 +0300

    Начал добавление второй активности

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn1(View view) {
+        Intent intent = new Intent(this, ShowData.class);
+        startActivity(intent);
+    }
+
+    public void onClickBtn2(View view) {
+
+    }
+
+    public void onClickBtn3(View view) {
+
     }
 }
